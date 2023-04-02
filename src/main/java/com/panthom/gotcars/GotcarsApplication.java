21d19c37461e2f766058f59398348e93db76d6dc
Joel Almeida <joel.almeida.sa@gmail.com>
2023-04-01 22:06:36 -0300

    first commit.

---
+++ src/main/java/com/panthom/gotcars/GotcarsApplication.java
@@ -0,0 +1,13 @@
+package com.panthom.gotcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotcarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotcarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panthom/gotcars/GotcarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panthom.gotcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotcarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
